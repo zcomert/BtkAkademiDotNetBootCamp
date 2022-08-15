829540479dc661ca27cd25c2f043f68275653f58
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-15 15:01:03 +0300

    Person has been defined.

--- day-01/index.js
+++ day-01/index.js
@@ -1,13 +1,27 @@
 console.log("Hello world.");
 
-document.getElementById("root").innerHTML = 'Merhaba';
+document.getElementById("root").innerHTML = "Merhaba";
 
-let i = 1;
-while (i<10) {
-    
-    const element = document.createElement("div");
-    element.innerHTML = i;
-    document.getElementById("root").append(element);
-    console.log(i);
-    i = i +1;
+const arr = [
+  {
+    id: 1,
+    name: "Sude Akgün",
+    age: 19,
+  },
+  {
+    id: 2,
+    name: "Ahmet Güneş",
+    age: 25,
+  },
+  {
+    id: 3,
+    name: "Ömer Dağ",
+    age: 37,
+  },
+];
+
+for (let person of arr) {
+  const element = document.createElement("div");
+  element.innerHTML = `${person.id} ${person.name} ${person.age}`;
+  document.getElementById("root").append(element);
 }
