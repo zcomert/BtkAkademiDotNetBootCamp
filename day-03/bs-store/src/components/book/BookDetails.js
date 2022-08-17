bff9a3f3f146eede64b0f021ce06eac53ad90a90
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-17 16:30:13 +0300

    useEffect hook has been done.

--- day-03/bs-store/src/components/book/BookDetails.js
+++ day-03/bs-store/src/components/book/BookDetails.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import {useParams} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import data from "../../data";
 
 export default function BookDetails() {
-    const {id} = useParams();
-     
+  const { id } = useParams();
+  const initial = {
+    id: 0,
+    title: "xx",
+    price: 0,
+    summary: "..."
+  }
+  const [book, setBook] = useState(initial);
+
+  useEffect(() => {
+    setBook(data.find((book) => book.id === parseInt(id)) || initial);
+  }, []);
+
   return (
-    <div>BookDetails {id}</div>
-  )
+    <div>
+      <h3>{book.title}</h3>
+      <p>{book.summary}</p>
+      <h5>
+        <b>Price</b> {book.price}
+      </h5>
+    </div>
+  );
 }
