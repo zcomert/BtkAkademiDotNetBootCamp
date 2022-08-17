701af6e653f7837f36252cd040f8f4d6dd0a3be1
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-17 11:48:16 +0300

    Book component has been designed.

--- day-03/bs-store/src/components/bookList/BookList.js
+++ day-03/bs-store/src/components/bookList/BookList.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Book from "../book/Book";
 
 export default function BookList({ books }) {
-  return <div>BookList {books.length}</div>;
+  return (
+    <>
+      <div>BookList {books.length}</div>
+      {books.map((book) => (
+        <Book book={book} />
+      ))}
+    </>
+  );
 }
