e8752d725b610a77b86d39485b92e4386835a386
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-18 12:35:56 +0300

    Fab has been added to booklist.

--- day-04/bs-store/src/components/bookList/BookList.js
+++ day-04/bs-store/src/components/bookList/BookList.js
@@ -1,15 +1,29 @@
-import {
-  Container,
-  List,
-  Typography,
-} from "@mui/material";
+import { Container, Fab, List, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import React from "react";
 import Book from "../book/Book";
 import BookAdd from "../book/BookAdd";
 
 export default function BookList({ books, removeBook, addBook }) {
+  const fabStyle = {
+    position: "absolute",
+    bottom: 16,
+    right: 16,
+  };
+
+  const fab = {
+    color: "error",
+    sx: fabStyle,
+    icon: <AddIcon />,
+    label: "Add",
+  };
+
   return (
-    <Container maxWidth="md">
+    <Container maxWidth='md'>
+      <Fab sx={fab.sx} color={fab.color} aria-label='add'>
+        {fab.icon}
+      </Fab>
+
       <Typography align='center' variant='h5' gutterBottom>
         Book List
       </Typography>
@@ -19,8 +33,6 @@ export default function BookList({ books, removeBook, addBook }) {
           <Book key={book.id} book={book} removeBook={removeBook} />
         ))}
       </List>
-      
-     
 
       <Typography align='center' variant='caption' display='block' gutterBottom>
         The numbe of books ({books.length}) has been listed.
