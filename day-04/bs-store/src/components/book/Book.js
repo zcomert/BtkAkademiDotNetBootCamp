0b4a6c4d2ec0b0aa91b0f17af2c4209ac8e710c1
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-18 10:08:33 +0300

    Material UI has been installed.

--- day-04/bs-store/src/components/book/Book.js
+++ day-04/bs-store/src/components/book/Book.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Button } from "@mui/material";
 import {Link} from "react-router-dom"
 
 export default function Book({ book, removeBook }) {
   return (
     <div>
       <Link to={`/books/details/${book.id}`}>{book.title}</Link>
-      <button onClick={() => removeBook(book.id)}>Remove</button>
+      <Button 
+      onClick={() => 
+      removeBook(book.id)}>Remove
+      </Button>
     </div>
   );
 }
