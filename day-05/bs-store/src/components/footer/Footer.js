4f8fdef28a3419c4649f93ccf8cdc7ac8ff7d6e9
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-19 14:55:51 +0300

    Footer has been done.

--- day-05/bs-store/src/components/footer/Footer.js
+++ day-05/bs-store/src/components/footer/Footer.js
@@ -7,9 +7,10 @@ import {
   ListItem,
   ListItemAvatar,
   Paper,
+  Typography,
 } from "@mui/material";
 import styles from "./style.module.css";
-import { Stack } from "@mui/system";
+import { Container, Stack } from "@mui/system";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -44,53 +45,102 @@ export default function Footer() {
     },
   ];
 
+  const personLinks = [
+    {
+      id: 1,
+      name: "Administrator",
+      url: "#",
+      avatar: "A",
+    },
+    {
+      id: 2,
+      name: "Manager",
+      url: "#",
+      avatar: "M",
+    },
+    {
+      id: 3,
+      name: "Lecture",
+      url: "#",
+      avatar: "L",
+    },
+    {
+      id: 4,
+      name: "Developers",
+      url: "#",
+      avatar: "D",
+    },
+  ];
+
   return (
-    <Grid container spacing={3} className={styles.footer}>
-      <Grid item xs={12} md={3}>
-        <Stack alignItems={"center"} justifyContent={"center"}>
-          <img
-            alt='BTK Akademi'
-            src='https://assets-btkakademi-gov-tr.akamaized.net/api/service/v1/public/51/assets/dm_logo.png?ts=1645167484961'
-          ></img>
-        </Stack>
-      </Grid>
+    <>
+      <Grid container spacing={3} className={styles.footer}>
+        <Grid item xs={12} md={3}>
+          <Stack alignItems={"center"} justifyContent={"center"}>
+            <img
+              alt='BTK Akademi'
+              src='https://assets-btkakademi-gov-tr.akamaized.net/api/service/v1/public/51/assets/dm_logo.png?ts=1645167484961'
+            ></img>
+          </Stack>
+        </Grid>
 
-      <Grid item xs={12} md={3}>
-        <List>
-          {links.map((link) => (
-            <ListItem key={link.id}>
-              <ListItemAvatar>
-                <Avatar sx={{ width: 24, height: 24 }}>{link.avatar}</Avatar>
-              </ListItemAvatar>
-              {link.name}
-            </ListItem>
-          ))}
-        </List>
-      </Grid>
+        <Grid item xs={12} md={3}>
+          <List>
+            {links.map((link) => (
+              <ListItem key={link.id}>
+                <ListItemAvatar>
+                  <Avatar sx={{ width: 24, height: 24 }}>{link.avatar}</Avatar>
+                </ListItemAvatar>
+                {link.name}
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
 
-      <Grid item xs={12} md={3}>
-        <Paper elevation={5}>3</Paper>
-      </Grid>
+        <Grid item xs={12} md={3}>
+          {personLinks.map((link) => {
+            // destructing
+            const { id, name } = link;
+            return <ListItem key={id}>{name}</ListItem>;
+          })}
+        </Grid>
 
-      <Grid item xs={12} md={3}>
-        <Stack justifyContent={"center"} spacing={2} direction='row'>
-          <IconButton>
-            <FacebookIcon />
-          </IconButton>
-          <IconButton>
-            <TwitterIcon />
-          </IconButton>
-          <IconButton>
-            <LinkedInIcon />
-          </IconButton>
-          <IconButton>
-            <GitHubIcon />
-          </IconButton>
-          <IconButton>
-            <InstagramIcon />
-          </IconButton>
-        </Stack>
+        <Grid item xs={12} md={3}>
+          <Stack
+            justifyContent={"center"}
+            alignItems={"center"}
+            spacing={2}
+            direction='row'
+          >
+            <IconButton>
+              <FacebookIcon />
+            </IconButton>
+            <IconButton>
+              <TwitterIcon />
+            </IconButton>
+            <IconButton>
+              <LinkedInIcon />
+            </IconButton>
+            <IconButton>
+              <GitHubIcon />
+            </IconButton>
+            <IconButton>
+              <InstagramIcon />
+            </IconButton>
+          </Stack>
+
+          <Stack spacing={3}>
+            <Typography sx={{ mt: 3 }} variant='caption' align='left'>
+              Bilgi Teknolojileri ve İletişim Kurumu Eskişehir Yolu 10.Km No:
+              276 Posta Kodu: 06530 Çankaya/Ankara
+              <br />
+              E-posta: destek@btkakademi.gov.tr
+              <br />
+              Tel: 0 (312) 294 72 00
+            </Typography>
+          </Stack>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
