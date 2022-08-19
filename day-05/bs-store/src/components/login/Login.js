7f7d332c8b24d276f14d3b64ecce9612df07257a
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-19 11:45:11 +0300

    Login component has been updated.

--- day-05/bs-store/src/components/login/Login.js
+++ day-05/bs-store/src/components/login/Login.js
@@ -1,21 +1,61 @@
+import {
+  Button,
+  Container,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useContext } from "react";
 import AppContext from "../../context/ContextApplication";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import HttpsIcon from "@mui/icons-material/Https";
+import { Stack } from "@mui/system";
 
 export default function Login() {
-  const {setIsLogin} = useContext(AppContext);
+  const { setIsLogin } = useContext(AppContext);
 
   return (
-    <div>
-      <h3>Login</h3>
-      <input type='text' />
-      <input type='password' />
+    <Container maxWidth='sm' sx={{ mt: 3 }}>
+      <Stack spacing={3}>
+        <Typography variant='h4' align='center' gutterBottom>
+          Login
+        </Typography>
 
-      <button onClick={
-        () => { 
-         setIsLogin(true)
-        }
-      } >Login</button>
-    
-    </div>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <AccountCircleIcon />
+              </InputAdornment>
+            ),
+          }}
+          placeholder='Username'
+          label='Username'
+          type='text'
+        />
+
+        <TextField
+          placeholder='Password'
+          label='Password'
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <HttpsIcon />
+              </InputAdornment>
+            ),
+          }}
+          type='password'
+        />
+
+        <Button
+          variant='contained'
+          onClick={() => {
+            setIsLogin(true);
+          }}
+        >
+          Login
+        </Button>
+      </Stack>
+    </Container>
   );
 }
