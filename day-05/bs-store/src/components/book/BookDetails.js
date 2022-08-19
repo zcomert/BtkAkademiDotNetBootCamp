875744ff8b3e9ad8b4fbe0c76b86e9c7ade73e3e
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-19 15:54:27 +0300

    BookDetails have been done.

--- day-05/bs-store/src/components/book/BookDetails.js
+++ day-05/bs-store/src/components/book/BookDetails.js
@@ -1,4 +1,12 @@
-import { Button, Container, Grid, Paper, Typography } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import data from "../../data";
@@ -20,17 +28,29 @@ export default function BookDetails() {
 
   return (
     <Container sx={{ mt: 3 }} maxWidth='lg'>
-      <Grid container>
+      <Grid container spacing={3}>
         <Grid item xs={12} md={4}>
-          <img src={`/books/${id}.jpg`} alt={book.title} />
-          <Button variant='outlined' onClick={() => navigate("/books/list")}>
-            Go to Book List{" "}
-          </Button>
+          <Stack spacing={2}>
+            <img src={`/books/${id}.jpg`} alt={book.title} />
+            <ButtonGroup variant='outlined'>
+              <Button
+                
+                onClick={() => navigate("/books/list")}
+              >
+                Go to Book List{" "}
+              </Button>
+              <Button>Add Cart</Button>
+            </ButtonGroup>
+          </Stack>
         </Grid>
         <Grid item xs={12} md={8}>
           <Typography variant='h4'>{book.title}</Typography>
-          <Typography variant='body1'>{book.summary}</Typography>
-          <Paper elevation={3}>{book.price}</Paper>
+          <Typography sx={{ padding: 5 }} variant='body1'>
+            "{book.summary}"
+          </Typography>
+          <Typography sx={{ color: "#f00" }} variant='h5'>
+            $ {book.price}
+          </Typography>
         </Grid>
       </Grid>
     </Container>
