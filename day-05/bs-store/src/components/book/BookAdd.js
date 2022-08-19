aec1c2964eae687d5191727526410b821e711b11
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-19 16:05:49 +0300

    Book add form is ongoing...

--- day-05/bs-store/src/components/book/BookAdd.js
+++ day-05/bs-store/src/components/book/BookAdd.js
@@ -1,25 +1,26 @@
+import { Button, Container, Stack, TextField } from "@mui/material";
 import React, { useContext } from "react";
 import { AppContext } from "../../context/ContextApplication";
 
 export default function BookAdd() {
   // destructing...
   const { books, setBooks } = useContext(AppContext);
- 
-  const book = {
-    id: 14,
-    title: "Nutuk",
-    price: 110,
-  };
-
-  const addBook = (book) => {
-    setBooks([...books, book]);
-  }
-
 
   return (
-    <div>
-      BookAdd
-      <button onClick={() => addBook(book)}>Add</button>
-    </div>
+    <Container maxWidth='md'>
+      <Stack spacing={2}>
+        <TextField id='title' label='Title' placeHolder='title'></TextField>
+
+        <TextField id='price' label='Price' placeHolder='Price'></TextField>
+
+        <TextField 
+        multiline
+        id='summary' 
+        label='Summary' 
+        placeHolder='Summary'></TextField>
+
+        <Button variant='contained'>Add</Button>
+      </Stack>
+    </Container>
   );
 }
