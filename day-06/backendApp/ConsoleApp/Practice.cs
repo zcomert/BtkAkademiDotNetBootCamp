dcc092abd3a44328df65d28bef29442392156a99
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-22 16:15:23 +0300

    ArrayList has been done.

--- day-06/backendApp/ConsoleApp/Practice.cs
+++ day-06/backendApp/ConsoleApp/Practice.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp;
+using System.Collections;
 
 public class Practice
 {
@@ -68,5 +69,38 @@ public class Practice
         Console.WriteLine(math.Sum(10));                        // 10
         Console.WriteLine(math.Sum(1, 3, 4, 5));                // 13
         Console.WriteLine(math.Sum(1, 3, 4, 5, 10, 30));        // 53
+    }
+
+    public void UseArrayList()
+    {
+        var bookList = new ArrayList();
+
+        bookList.Add(new Book(1, "Book 1"));
+        bookList.Add(new Book(2, "Book 2"));
+        bookList.Add(new Book(3, "Book 3"));
+        bookList.Add("Ömer");
+        bookList.Add(true);
+        bookList.Add(DateTime.Now);
+        bookList.Add(25);
+        bookList.Add(5 / 2);
+
+        bookList.AddRange(new int[] { 1, 2, 3 });
+
+        Console.WriteLine(String.Format("Number of item in collection : {0}", bookList.Count));
+
+        foreach (var item in bookList)
+        {
+            Console.WriteLine(item);
+        }
+
+        bookList.Remove(true);
+        bookList.RemoveAt(0);
+        bookList.RemoveRange(0, 3);
+        Console.WriteLine(String.Format("Number of item in collection : {0}", bookList.Count));
+
+        foreach (var item in bookList)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
--- day-06/backendApp/ConsoleApp/Program.cs
+++ day-06/backendApp/ConsoleApp/Program.cs
@@ -1,11 +1,49 @@
 ﻿
+using ConsoleApp;
+using System.Collections;
 
-Console.WriteLine(new ConsoleApp.Math.Math().Sum(1,2,3)); //6 
+var bookList = new ArrayList();
+
+bookList.Add(new Book(1, "Book 1"));
+bookList.Add(new Book(2, "Book 2"));
+bookList.Add(new Book(3, "Book 3"));
+bookList.Add("Ömer");
+bookList.Add(true);
+bookList.Add(DateTime.Now);
+bookList.Add(25);
+bookList.Add(5 / 2);
+
+bookList.AddRange(new int[] { 1, 2, 3 });
+
+Console.WriteLine(String.Format("Number of item in collection : {0}",bookList.Count));
+
+foreach (var item in bookList)
+{
+    Console.WriteLine(item);
+}
+
+bookList.Remove(true);
+bookList.RemoveAt(0);
+bookList.RemoveRange(0, 3);
+Console.WriteLine(String.Format("Number of item in collection : {0}", bookList.Count));
+
+foreach (var item in bookList)
+{
+    Console.WriteLine(item);
+}
+Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
 
 //var practice = new Practice();
 //practice.ArrayDefination();
 //practice.UseMathInProgram();
 //practice.UseParamsKeyword();
-
-
-Console.ReadKey();
