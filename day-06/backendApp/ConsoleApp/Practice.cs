b9e2f4444c2f87b6b2650b6a808c8435425706f2
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-22 14:47:57 +0300

    Practice class has been defined.

---
+++ day-06/backendApp/ConsoleApp/Practice.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp;
+
+public class Practice
+{
+    public void ArrayDefination()
+    {
+        var BookArr =
+        new Book[] {
+        new Book(1,"Devlet"),
+        new Book(){Id = 2, Title = "Yoldaki İşaretler"},
+        new Book { Id=3, Title = "C# Hakkında Herşey"}
+        };
+
+        for (int i = 0; i < BookArr.Length; i++)
+        {
+            Console.WriteLine(BookArr[i]);
+        }
+
+        BookArr[1].Id = 30;
+        BookArr[1].Title = "Mars";
+
+        Console.WriteLine();
+        foreach (var book in BookArr)
+        {
+            Console.WriteLine(book);
+        }
+
+        // mutable (değişebilir) -  immutable (değişemez)
+
+        var newBookArr = new Book[5];
+
+        for (int i = 0; i < BookArr.Length; i++)
+        {
+            newBookArr[i] = BookArr[i];
+        }
+        newBookArr[3] = new Book(4, "Sınırsız Güç");
+        newBookArr[4] = new Book(5, "Dijital Kale");
+
+        foreach (var item in newBookArr)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
+    }
+}
--- day-06/backendApp/ConsoleApp/Program.cs
+++ day-06/backendApp/ConsoleApp/Program.cs
@@ -1,18 +1,4 @@
-﻿using ConsoleApp;
+﻿Console.ReadKey();
 
-Book book1 = new Book();
-book1.Id = 1;
-book1.Title = "Devlet";
-
-Book book2 = new Book(2, "Kitab-ı Aşk");
-Book book3 = new Book() { Id = 3, Title = "Mars" };
-
-var book4 = new Book() { Id = 4, Title = "Yol" };
-
-
-Console.WriteLine(book1);
-Console.WriteLine(book2);
-Console.WriteLine(book3);
-Console.WriteLine(book4);
-
-Console.ReadKey();
+var practice = new Practice();
+practice.ArrayDefination();
