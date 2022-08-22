a1eea7f3adaf97486fd2b525479a0f1199c8e7e4
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-22 12:25:48 +0300

    Book object has been created.

---
+++ day-06/backendApp/ConsoleApp/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Book
+    {
+        // Auto-implemented property
+        public int Id { get; set; }
+        public String Title { get; set; }
+
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
---
+++ day-06/backendApp/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using ConsoleApp;
+
+Book book1 = new Book();
+book1.Id = 1;
+book1.Title = "Devlet";
+
+Book book2 = new Book(2, "Kitab-ı Aşk");
+Book book3 = new Book() { Id = 3, Title = "Mars" };
+
+var book4 = new Book() { Id = 4, Title = "Yol" };
+
+
+Console.WriteLine(book1.Id);
+Console.WriteLine(book1.Title);
+
+Console.WriteLine(book2.Id);
+Console.WriteLine(book2.Title);
+
+Console.WriteLine(book3.Id);
+Console.WriteLine(book3.Title);
+
+Console.WriteLine(book4);
+
+Console.ReadKey();
