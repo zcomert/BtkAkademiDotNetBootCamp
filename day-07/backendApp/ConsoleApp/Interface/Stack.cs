68c84c289aaef8adf7a14304228407a69b856f7c
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-23 13:54:08 +0300

    Stack is ongoing...

---
+++ day-07/backendApp/ConsoleApp/Interface/Stack.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp.Interface
+{
+    public class Stack<T> : IStack<T>
+    {
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
