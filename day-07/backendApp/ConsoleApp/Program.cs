abbde3e6036ef636b48800679eccfee2ec366d89
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-23 10:32:35 +0300

    Inheritance sample is ongoing...

--- day-07/backendApp/ConsoleApp/Program.cs
+++ day-07/backendApp/ConsoleApp/Program.cs
@@ -1,9 +1,11 @@
-﻿var list1 = new List<String>
-{
-    "Ahmet","Sade","Ömer Faruk"
-};
+﻿using ConsoleApp.Inheritance;
 
-var list2 = list1;
+var shape1 = new Shape() { X = 3, Y = 4 };
+var shape2 = new Rectangle { X = 30, Y = 40 };
+var shape3 = new Circle { X = -4, Y = -8 };
+
+shape1.Draw();
+shape2.Draw();
+shape3.Draw();
 
 Console.ReadKey();
-
---
+++ day-07/backendApp/ConsoleApp/RefandValueType/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.RefandValueType
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+       
+
+        public override string ToString()
+        {
+            return $"X={X} Y={Y}";
+        }
+    }
+}
