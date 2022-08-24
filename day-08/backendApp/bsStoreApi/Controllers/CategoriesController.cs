585f062d5dba4d87371d7dac77f1d6cd4cbb3077
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-24 17:36:36 +0300

    Day-08 has been completed.

--- day-08/backendApp/bsStoreApi/Controllers/CategoriesController.cs
+++ day-08/backendApp/bsStoreApi/Controllers/CategoriesController.cs
@@ -1,6 +1,36 @@
-﻿namespace bsStoreApi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories.Concrete;
+
+namespace bsStoreApi.Controllers
 {
-    public class CategoriesController
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
     {
+        private AppDbContext _context;
+        public CategoriesController()
+        {
+            _context = new AppDbContext();
+        }
+        
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Ok(_context.Categories?.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneCategory([FromRoute] int id)
+        {
+            var category = _context
+                .Categories?
+                .Where(c => c.CategoryId == id)
+                .SingleOrDefault();
+
+            if (category is null)
+                throw new Exception("Category not found.");
+
+            return Ok(category);
+        }
     }
 }
