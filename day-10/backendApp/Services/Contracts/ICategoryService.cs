5fb712fce80c89d5b7976a04c7734dbe4fccf779
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-26 14:48:37 +0300

    CategoryService is ongoing...

--- day-10/backendApp/Services/Contracts/ICategoryService.cs
+++ day-10/backendApp/Services/Contracts/ICategoryService.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Services.Contracts
 {
-    internal interface ICategoryService
+    public interface ICategoryService
     {
+        List<Category> GetAllCategories(Expression<Func<Category, bool>> filter = null);
+        Category GetOneCategory(int categoryId);
+
+        Category CreateOneCategory(Category category);
+        Category UpdateOneCategory(int categoryId, Category author);
+        void DeleteOneCategory(int categoryId);
     }
 }
