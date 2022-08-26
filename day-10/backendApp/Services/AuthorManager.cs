4d20e01e5f12a78e0df8eedf8029cd8b9f1b9170
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-26 13:55:23 +0300

    AuthorService is ongoing...

---
+++ day-10/backendApp/Services/AuthorManager.cs
@@ -0,0 +1,47 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AuthorManager : IAuthorService
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorManager(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public Author CreateOneAuthor(Author author)
+        {
+           if(author is null)
+            {
+                throw new ArgumentNullException("Author is empty.");
+            }
+           _authorRepository.Add(author);
+            return author;
+        }
+
+        public List<Author> GetAllAuthors(Expression<Func<Author, bool>> filter = null)
+        {
+            return _authorRepository.GetAll(filter);
+        }
+
+        public Author GetOneAuthor(int id)
+        {
+            var author = _authorRepository.Get(a => a.AuthorId == id);
+            if (author is null)
+            {
+                throw new Exception("Author could not found.");
+            }
+            return author;
+        }
+    }
+}
---
+++ day-10/backendApp/Services/Contracts/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IAuthorService
+    {
+        List<Author> GetAllAuthors(Expression<Func<Author,bool>> filter=null);
+        Author GetOneAuthor(int id);
+
+        Author CreateOneAuthor(Author author);
+    }
+}
