f797b972ad09050d7d2459d49d0824de31221787
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-26 10:06:07 +0300

    Presentation Layer has been added.

--- day-10/backendApp/bsStoreApi/Program.cs
+++ day-10/backendApp/bsStoreApi/Program.cs
@@ -6,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(bsStoreApi.Presentation.AssemblyReference).Assembly);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
