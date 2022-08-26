a832339289f508b5071c6bec9ff3c8ee47fd2794
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-26 16:47:42 +0300

    GlobalError management.

---
+++ day-10/backendApp/Entities/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Exceptions
+{
+    public class BookNotFoundException : NotFoundException
+    {
+        public BookNotFoundException(int id) : 
+            base($"Book with {id} could not found.")
+        {
+
+        }
+    }
+}
---
+++ day-10/backendApp/Entities/Exceptions/NotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(String message) : base(message)
+        {
+
+        }
+    }
+
+    public class AuthorNotFoundException : NotFoundException
+    {
+        public AuthorNotFoundException(int id)
+            : base($"Author with {id} id could not found.")
+        {
+
+        }
+    }
+}
