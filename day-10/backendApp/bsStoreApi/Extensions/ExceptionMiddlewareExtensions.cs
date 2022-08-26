9c3361e34d01c6805a97065991ab7eb153f9144d
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-26 16:11:19 +0300

    GlobalError management.

--- day-10/backendApp/bsStoreApi/Extensions/ExceptionMiddlewareExtensions.cs
+++ day-10/backendApp/bsStoreApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,6 +1,31 @@
-﻿namespace bsStoreApi.Extensions
+﻿using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace bsStoreApi.Extensions
 {
-    public class ExceptionMiddlewareExtensions
+    public static class ExceptionMiddlewareExtensions
     {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appErr =>
+            {
+                appErr.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature!=null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internatl Server Error"
+                        }.ToString());
+                    }
+                });
+            });
+        }
     }
 }
