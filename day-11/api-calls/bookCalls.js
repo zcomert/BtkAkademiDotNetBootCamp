ce14c27636444926419f123af8647bfd858c8e29
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-29 15:57:41 +0300

    api-calls for Book

--- day-11/api-calls/bookCalls.js
+++ day-11/api-calls/bookCalls.js
@@ -1,7 +1,22 @@
 import fetch from "node-fetch";
 
-const
-
-fetch("http://localhost:44299/api/books")
+const getAllBooksAsync = async () => {
+    return fetch("http://localhost:44299/api/books")
     .then(resp => resp.json())
-    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+}
+
+const getAllBooks = () => {
+    return fetch("http://localhost:44299/api/books")
+    .then(resp => resp.json())
+    .catch(err => console.log(err));
+}
+
+const dataAsync = await getAllBooksAsync();
+console.log(dataAsync);
+
+const data = await getAllBooks();
+data.then(resp => resp.data);
+
+
+
