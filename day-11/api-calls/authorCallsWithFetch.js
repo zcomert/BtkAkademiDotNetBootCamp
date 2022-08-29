fe0b4a4270969d5314d3a3573889f2e15867a595
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-29 17:20:26 +0300

    api-calls for Book

--- day-11/api-calls/authorCallsWithFetch.js
+++ day-11/api-calls/authorCallsWithFetch.js
@@ -1,26 +1,54 @@
 import fetch from "node-fetch";
 
 const getAllAuthors = () => {
-  return fetch("http://localhost:44299/api/authors")
-  .then((resp) => resp.json());
+  return fetch("http://localhost:44299/api/authors").then((resp) =>
+    resp.json()
+  );
 };
 
 const createOneAuthor = () => {
+  const methodbody = {
+    firstName: "Ahmet",
+    lastName: "Hakan",
+  };
 
-    const methodbody = {
-        "firstName":"Ahmet",
-        "lastName":"Hakan"
-    };
+  // post request
+  return fetch("http://localhost:44299/api/authors", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(methodbody),
+  }).then((resp) => resp.json());
+};
 
-    // post request
-    return fetch("http://localhost:44299/api/authors",{
-        method : 'POST',
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify(methodbody)
-    }).then(resp => resp.json());
+const updateOneAuthor = () => {
+  const methodbody = {
+    authorId: 7,
+    firstName: "Senai",
+    lastName: "Demirci",
+  };
+
+  return fetch("http://localhost:44299/api/authors/7", {
+    method: "PUT",
+    body: JSON.stringify(methodbody),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((resp) => resp.json());
+};
+
+const deleteOneAuthor = (id) => {
+    const url = `http://localhost:44299/api/authors/${id}`;
+    return fetch(url,{
+        method:'DELETE',
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    }).then(resp => resp);
 }
 
-createOneAuthor().then(resp => console.log(resp));
-getAllAuthors().then(resp => console.log(resp))
+// createOneAuthor().then((resp) => console.log(resp));
+// updateOneAuthor().then((resp) => console.log(resp));
+deleteOneAuthor(6);
+getAllAuthors().then((resp) => console.log(resp));
