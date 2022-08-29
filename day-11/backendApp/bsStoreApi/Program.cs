3fd4d961d593036fe230481fda1102951b1e1211
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-29 10:47:59 +0300

    OneToOne relation is ongoing...

--- day-11/backendApp/bsStoreApi/Program.cs
+++ day-11/backendApp/bsStoreApi/Program.cs
@@ -7,9 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers()
-    .AddJsonOptions(opt =>
-                opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
-    .AddApplicationPart(typeof(bsStoreApi.Presentation.AssemblyReference).Assembly);
+    .AddApplicationPart(typeof(bsStoreApi.Presentation.AssemblyReference).Assembly)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
