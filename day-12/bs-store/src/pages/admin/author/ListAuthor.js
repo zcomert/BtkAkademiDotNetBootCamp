2ae24c53c9da805e9554b111e959552e1373269a
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-30 16:24:40 +0300

    Author admin page is ongoing...

--- day-12/bs-store/src/pages/admin/author/ListAuthor.js
+++ day-12/bs-store/src/pages/admin/author/ListAuthor.js
@@ -1,4 +1,15 @@
-import { Container } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  ButtonGroup,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllAuthors } from "../../../store/actions/authorActions";
@@ -10,5 +21,41 @@ export default function ListAuthor() {
   useEffect(() => {
     dispatch(getAllAuthors());
   }, []);
-  return <Container maxWidth='md'>{authors?.length}</Container>;
+  return (
+    <Container maxWidth='md'>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {authors?.map((author) => (
+              <TableRow>
+                <TableCell>{author.authorId}</TableCell>
+                <TableCell>
+                  <Avatar 
+                  src={`/authors/${author.authorId%21}.jpg`}
+                  alt={`${author.firstName} ${author.lastName}`} />
+                </TableCell>
+                <TableCell>{author.firstName}</TableCell>
+                <TableCell>{author.lastName}</TableCell>
+                <TableCell align="right">
+                  <ButtonGroup orientation="vertical">
+                    <Button>Update</Button>
+                    <Button>Remove</Button>
+                  </ButtonGroup>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
