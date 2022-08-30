e9c88661ee31ed524cad84f0f719dc741d907e1c
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-30 14:55:29 +0300

    Author-Redux is ongoing...

--- day-12/bs-store/src/store/reducers/authorReducer.js
+++ day-12/bs-store/src/store/reducers/authorReducer.js
@@ -1,3 +1,19 @@
-export default function authorReducer(){
-    
+import { GETALL_AUTHORS } from "../actions/authorActions";
+import { authors } from "../initials/authorItems";
+
+const initialVales = {
+  authors,
+};
+export default function authorReducer(state = initialVales, { type, payload }) {
+  switch (type) {
+    case GETALL_AUTHORS:
+      return {
+        ...state, // author
+        authors: payload,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
 }
