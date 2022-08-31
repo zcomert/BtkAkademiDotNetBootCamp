fe9b884379f3b3d70355f4c18faed28e9a25f729
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-31 14:04:27 +0300

    Author update is ongoing...

--- day-13/bs-store/src/pages/admin/author/UpdateAuthor.js
+++ day-13/bs-store/src/pages/admin/author/UpdateAuthor.js
@@ -1,20 +1,59 @@
-import { Typography } from '@mui/material'
-import { Container } from '@mui/system';
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import SimpleFab from '../../../components/fab/SimpleFab';
-import AuthorService from '../../../services/authorService';
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import { useFormik } from "formik";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import SimpleFab from "../../../components/fab/SimpleFab";
+import AuthorService from "../../../services/authorService";
 
 export default function UpdateAuthor() {
-    const {id} = useParams();
-    const authorService = new AuthorService();
-    const [author, setAuthor] = useState();
+  
+  const { id } = useParams();
+  const authorService = new AuthorService();
+
+  useEffect(() => {
+    authorService.getOneAuthor(id).then((resp) => {
+      setValues(resp);
+     });
+  }, []);
+
+  const { handleSubmit, handleChange, values, setValues } = useFormik({
+    initialValues : {
+      firstName : '',
+      lastName : ''
+    },
+    onSubmit : () => {
+      console.log(values);
+    }
+  });
+ 
+
   return (
-    <Container maxWidth="md">
-      <SimpleFab url="/admin/authors/list" />
-      <form>
-        
+    <Container maxWidth='md'>
+      <SimpleFab url='/admin/authors/list' />
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            id='firstName'
+            name='firstName'
+            fullWidth
+            onChange={handleChange}
+            value = {values.firstName}
+          ></TextField>
+
+          <TextField
+            id='lastName'
+            name='lastName'
+            fullWidth
+            onChange={handleChange}
+            value = {values.lastName}
+          ></TextField>
+
+          <Button type='submit' variant='contained'>
+            Save
+          </Button>
+        </Stack>
       </form>
     </Container>
-  )
+  );
 }
