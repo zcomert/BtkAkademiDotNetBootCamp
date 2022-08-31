9b58783af5cb0e92c9f60d52eb1e222c1845ca1a
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-31 13:20:23 +0300

    Author Update is ongoing...

--- day-13/bs-store/src/pages/admin/author/UpdateAuthor.js
+++ day-13/bs-store/src/pages/admin/author/UpdateAuthor.js
@@ -1,13 +1,15 @@
 import { Typography } from '@mui/material'
+import { Container } from '@mui/system';
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function UpdateAuthor() {
+    const {id} = useParams();
   return (
-    <div>
-      ...
-      <Typography variant="h1">
-        Selam
-      </Typography>
-    </div>
+    <Container maxWidth="md">
+      <form>
+        
+      </form>
+    </Container>
   )
 }
