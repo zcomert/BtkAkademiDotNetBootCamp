578c2c59d0348c9fa86ad09ffdb38c49ed2f2106
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-09-01 12:19:03 +0300

    AddCategory design is ongoing...

--- day-14/bs-store/src/pages/admin/category/AddCategory.js
+++ day-14/bs-store/src/pages/admin/category/AddCategory.js
@@ -1,17 +1,36 @@
-import { Container, TextField } from "@mui/material";
+import { Button, Container, TextField } from "@mui/material";
 import { Stack } from "@mui/system";
+import { useFormik } from "formik";
 import React from "react";
+import { useDispatch } from "react-redux";
 import SimpleFab from "../../../components/fab/SimpleFab";
+import { createOneCategory } from "../../../store/actions/categoryActions";
+import { openSnackbar} from "../../../store/actions/appActions";
 
 export default function AddCategory() {
+  const dispatch = useDispatch();
+  const {handleSubmit, handleChange, values} = useFormik({
+    initialValues : {
+      categoryName:'',
+      description:''
+    },
+    onSubmit : (values) => {
+      dispatch(createOneCategory(values));
+      dispatch(openSnackbar({
+        message: `${values.categoryName} has been created.`,
+        severity: 'success'
+      }));
+    }
+  });
   return (
     <Container maxWidth='md'>
       <SimpleFab url='/admin/categories/list' />
-      <form>
+      <form onSubmit={handleSubmit} >
         <Stack spacing={3}>
           <TextField
             id='categoryName'
             name='categoryName'
+            onChange={handleChange}
             fullWidth
             label='Category name'
           ></TextField>
@@ -19,8 +38,12 @@ export default function AddCategory() {
             id='description'
             name='description'
             fullWidth
+            onChange={handleChange}
             label='Description'
           ></TextField>
+          <Button type='submit' variant='contained'>
+            Save
+          </Button>
         </Stack>
       </form>
     </Container>
