98aad7efda1a34dc0d6892ad76bdbdeaff8cf515
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-09-02 11:51:28 +0300

    Registration...

--- final/backendApp/bsStoreApi.Presentation/Controllers/AuthenticationController.cs
+++ final/backendApp/bsStoreApi.Presentation/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 
 namespace bsStoreApi.Presentation.Controllers
 {
@@ -7,6 +9,29 @@ namespace bsStoreApi.Presentation.Controllers
     public class AuthenticationController :
         ControllerBase
     {
+        private readonly IAuthService _authService;
 
+        public AuthenticationController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistrationDto)
+        {
+            var result = await _authService
+                .RegisterUser(userForRegistrationDto);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+
+            return StatusCode(201);
+        }
     }
 }
