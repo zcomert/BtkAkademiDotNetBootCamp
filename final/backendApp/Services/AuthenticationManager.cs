e46ea1bdae177653b996993b9ff82d599a8a5d23
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-09-02 13:11:26 +0300

    Methods...

--- final/backendApp/Services/AuthenticationManager.cs
+++ final/backendApp/Services/AuthenticationManager.cs
@@ -3,10 +3,13 @@ using Entities.Dtos;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Services.Contracts;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +21,7 @@ namespace Services
         private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
+        private User? _user;
 
         public AuthenticationManager(IMapper mapper, 
             UserManager<User> userManager, 
@@ -26,6 +30,16 @@ namespace Services
             _mapper = mapper;
             _userManager = userManager;
             _configuration = configuration;
+        }
+
+        public Task<string> CreateToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetOneUser(string username)
+        {
+            throw new NotImplementedException();
         }
 
         public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
@@ -40,6 +54,45 @@ namespace Services
                     .AddToRolesAsync(user, userForRegistration.Roles);
             }
             return result;
+        }
+
+        public Task<bool> ValidateUser(UserForAuthenticationDto userForAuth)
+        {
+            throw new NotImplementedException();
+        }
+
+        private SigningCredentials GetSigninCredentials()
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["secretKey"]);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, _user.UserName) };
+            var roles = await _userManager
+                .GetRolesAsync(_user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials,
+            List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var tokenOptions = new JwtSecurityToken(
+                issuer: jwtSettings["validIssuer"],
+                audience: jwtSettings["validAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+                signingCredentials: signingCredentials);
+            return tokenOptions;
         }
     }
 }
