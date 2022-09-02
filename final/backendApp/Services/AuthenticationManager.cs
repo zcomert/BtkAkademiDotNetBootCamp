af1e9bc4dd4b27085732d9a525d1d15a99fe6f6d
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-09-02 11:49:03 +0300

    Registration is ongoing...

--- final/backendApp/Services/AuthenticationManager.cs
+++ final/backendApp/Services/AuthenticationManager.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using Entities.Dtos;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,34 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    internal class AuthenticationManager
+    public class AuthenticationManager : IAuthService
     {
+        // DI 
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationManager(IMapper mapper, 
+            UserManager<User> userManager, 
+            IConfiguration configuration)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+           var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager
+                .CreateAsync(user, userForRegistration.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager
+                    .AddToRolesAsync(user, userForRegistration.Roles);
+            }
+            return result;
+        }
     }
 }
