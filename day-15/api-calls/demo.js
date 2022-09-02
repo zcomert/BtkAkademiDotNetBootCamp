4fc5e862f20e0ec513feea2b14d60203c2d24c31
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-09-02 15:27:57 +0300

    Day-15 has been completed.

--- day-15/api-calls/demo.js
+++ day-15/api-calls/demo.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"; 
 
 const getAllAuthors = async () => {
     const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiemNvbWVydCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hbmFnZXIiLCJleHAiOjE2NjIxMjEzOTksImlzcyI6ImJzdG9yZWFwaSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJ9.pVETDuraWn74-xSPRMZSrQoN4AHfQG2FCNC1TlfYurk";
