82016e3874d278b75d5bc10ec950f945eb91ff46
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-09-02 13:02:49 +0300

    IAuthService has been updated.

--- final/backendApp/Services/Contracts/IAuthService.cs
+++ final/backendApp/Services/Contracts/IAuthService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Dtos;
+using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace Services.Contracts
@@ -6,5 +7,8 @@ namespace Services.Contracts
     public interface IAuthService
     {
         Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
+        Task<User> GetOneUser(string username);
     }
 }
